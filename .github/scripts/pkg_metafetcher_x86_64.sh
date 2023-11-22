574dfb1dcc4e0b1257e6d7c6e871b179829a9a7f
Ajam <AjamX101@gmail.com>
2023-11-23 00:23:04 +0545

    sanity check - fail & exit if GH Token not exported

--- .github/scripts/pkg_metafetcher_x86_64.sh
+++ .github/scripts/pkg_metafetcher_x86_64.sh
@@ -11,6 +11,15 @@
 # Actions: BIN="$BIN" REPO="$REPO" SOURCE_BIN="Azathothas/Toolpacks" bash "$GITHUB_WORKSPACE/main/.github/scripts/pkg_metafetcher_x86_64.sh"
 # General: BIN="$BIN" REPO="$REPO" SOURCE_BIN="Azathothas/Toolpacks" bash <(curl -qfsSL "https://raw.githubusercontent.com/metis-os/hysp-pkgs/main/.github/scripts/pkg_metafetcher_x86_64.sh")
 
+#Sanity Checks for token
+if [[ -z "$GITHUB_TOKEN" ]]; then
+   # With Token = 5000 req/minute (80 req/minute)
+   # No Token = 60 request/hr
+   echo -e "\n[-] GITHUB_TOKEN is NOT Exported"
+   echo -e "Export it to avoid ratelimits"
+   exit 1
+fi
+
 #Fetch raw json
 PKG_METADATA="$(curl -qfsSL "https://api.github.com/repos/$SOURCE_BIN/contents/x86_64/$BIN" -H "Authorization: Bearer $GITHUB_TOKEN")" && export PKG_METADATA="$PKG_METADATA"
 REPO_METADATA="$(curl -qfsSL "https://api.github.com/repos/$REPO" -H "Authorization: Bearer $GITHUB_TOKEN")" && export REPO_METADATA="$REPO_METADATA"
