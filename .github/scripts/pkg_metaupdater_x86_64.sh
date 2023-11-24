e27fa933e0d9791ab6c27f4466ad3784bed908ab
Ajam <AjamX101@gmail.com>
2023-11-25 01:19:19 +0545

    line endings

--- .github/scripts/pkg_metaupdater_x86_64.sh
+++ .github/scripts/pkg_metaupdater_x86_64.sh
@@ -29,6 +29,7 @@ if [ ! -s "$GITHUB_WORKSPACE/main/data/x86_64/$BIN.toml" ]; then
    #Get the sample
    curl -qfsSL "https://raw.githubusercontent.com/metis-os/hysp-pkgs/main/data/x86_64/SAMPLE_SPEC.toml" -o "$GITHUB_WORKSPACE/main/data/x86_64/$BIN.toml"
    chmod +xwr "$GITHUB_WORKSPACE/main/data/x86_64/$BIN.toml"
+   dos2unix --quiet "$GITHUB_WORKSPACE/main/data/x86_64/$BIN.toml"
 fi
 
 #Edit
